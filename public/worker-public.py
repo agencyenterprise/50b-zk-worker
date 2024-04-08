c51337bbc4b1b1000642d18ce18c7df9fac6431f
kevinkons <kevin.kons@hotmail.com>
2024-04-08 15:05:10 -0300

    Changing worker url registration

--- public/worker-public.py
+++ public/worker-public.py
@@ -101,7 +101,7 @@ def register_worker():
     print('Worker Wallet: {}'.format(os.environ['WORKER_WALLET']))
     print('Public key: {}'.format(public_key), flush=True)
 
-    call_hub('Worker registration', '/worker/register', {
+    call_hub('Worker registration', '/workers/register', {
         'mode': mode,
         'url': os.environ['WORKER_URL'],
         'wallet': os.environ['WORKER_WALLET'],
