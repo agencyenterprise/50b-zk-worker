d134e5d9a70f91b0602a9b2a535d5eb541041912
Andre Borges Medeiros <andborges@gmail.com>
2024-04-10 10:58:33 -0300

    Add r1cs parameter to receiveWitness endpoint

--- public/worker-public.py
+++ public/worker-public.py
@@ -175,6 +175,7 @@ def healthcheck():
 @app.route('/receiveWitness', methods=['GET']) # Receive witness from the Hub
 def receiveWitness():
     jobId = request.args.get('jobId')
+    r1cs = request.args.get('r1cs')
     ciphered_witness = request.args.get('witness')
     ciphered_aeskey = request.args.get('key')
     iv = request.args.get('iv')
